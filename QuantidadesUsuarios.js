2c6d8e29cb18f0e3fb0ed1225984057bb74b3476
williankaue23 <136848764+williankaue23@users.noreply.github.com>
2024-10-28 15:22:25 -0300

    Delete QuantidadesUsuarios.js

--- QuantidadesUsuarios.js
+++
@@ -1,8 +0,0 @@
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-      
-      console.log(dados)
-  }
-  
